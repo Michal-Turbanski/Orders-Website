9749cb19e0373e1ca1dc45c4bc11312e185319a8
Michal-Turbanski <michalturbanski@gmail.com>
2022-05-23 18:13:55 +0200

    edit/PUT request work & all CRUD operations is  working

---
+++ public/js/sendPut.js
@@ -0,0 +1,25 @@
+const confirmEditButton = document.querySelector('button.confirmEditButton');
+const nameInput = document.querySelector('input.nameInput');
+const priceInput = document.querySelector('input.priceInput');
+const quantityInput = document.querySelector('input.quantityInput');
+
+confirmEditButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const nameValue = nameInput.value;
+    const priceValue = priceInput.value;
+    const quantityValue = quantityInput.value;
+
+    axios.put(`/api/orders/${confirmEditButton.value}`, {
+        name: nameValue,
+        price: priceValue,
+        quantity: quantityValue
+    })
+        .then(function (response) {
+            console.log(response);
+            window.location.href = "/";
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+})
