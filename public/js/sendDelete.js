070fbab1dccd098ff92532cc120c596d8e8e5a9e
Michal-Turbanski <michalturbanski@gmail.com>
2022-05-23 17:23:47 +0200

    delete order functionality added to front

---
+++ public/js/sendDelete.js
@@ -0,0 +1,15 @@
+const deleteButtons = document.querySelectorAll('button.deleteButton');
+
+
+deleteButtons.forEach((deleteButton) => {
+    deleteButton.addEventListener('click', () => {
+        axios.delete(`/api/orders/${deleteButton.value}`)
+            .then(function (response) {
+                console.log(response);
+                window.location.href = "/";
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    })
+})
