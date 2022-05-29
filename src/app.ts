2e07bd4d9dd5ce98e52d49e4fbeff3eee1326875
Michal-Turbanski <michalturbanski@gmail.com>
2022-05-29 14:04:31 +0200

    app.js rewrite

--- src/app.ts
+++ src/app.ts
@@ -3,21 +3,28 @@ const dotenv = require('dotenv').config();
 const colors = require('colors');
 const sequelize = require('./db/dbConnect');
 const path = require('path');
-const port = Number(process.env.PORT) || 3000;
+
+const port: number = Number(process.env.PORT) || 3000;
+
 sequelize.sync()
     // @ts-ignore
     .then(() => console.log('DB is ready'.cyan))
     // @ts-ignore
     .catch((err) => console.log(`${err}`.red));
+
 const app = express();
+
 app.set('view engine', 'ejs');
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/', require('./routes/mainRoute'));
-app.use('/create', require('./routes/createRoute'));
+app.use('/create', require('./routes/createRoute'))
 app.use('/edit', require('./routes/editRoute'));
+
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 });
