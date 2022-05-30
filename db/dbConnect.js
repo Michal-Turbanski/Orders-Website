eff81ebda7a792dc075e7a6bfb7d7c49548af413
Michal-Turbanski <michalturbanski@gmail.com>
2022-05-30 16:02:17 +0200

    refactor dbConnect

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,14 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sequelize = void 0;
-//@ts-ignore
 const { Sequelize } = require('sequelize');
 const dbName = process.env.DB_NAME || 'ordersDB';
 const dbUser = process.env.DB_USER || 'user';
 const dbPassword = process.env.DB_PASSWORD || 'password';
 const dbDialect = process.env.DB_DIALECT || 'sqlite';
 const dbHost = process.env.DB_HOST || './db/ordersDB.sqlite';
-//@ts-ignore
 exports.sequelize = new Sequelize(dbName, dbUser, dbPassword, {
     dialect: dbDialect,
     host: dbHost
--- src/db/dbConnect.ts
+++ src/db/dbConnect.ts
@@ -1,13 +1,11 @@
-//@ts-ignore
 const { Sequelize } = require('sequelize');
 
-const dbName = process.env.DB_NAME || 'ordersDB';
-const dbUser = process.env.DB_USER || 'user';
-const dbPassword = process.env.DB_PASSWORD || 'password';
-const dbDialect = process.env.DB_DIALECT || 'sqlite';
-const dbHost = process.env.DB_HOST || './db/ordersDB.sqlite';
+const dbName: string = process.env.DB_NAME || 'ordersDB';
+const dbUser: string = process.env.DB_USER || 'user';
+const dbPassword: string = process.env.DB_PASSWORD || 'password';
+const dbDialect: string = process.env.DB_DIALECT || 'sqlite';
+const dbHost: string = process.env.DB_HOST || './db/ordersDB.sqlite';
 
-//@ts-ignore
 export const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
     dialect: dbDialect,
     host: dbHost
