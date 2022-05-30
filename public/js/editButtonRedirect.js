7317c2bb6e7b9f27faa86e0fc370303e515a602a
Michal-Turbanski <michalturbanski@gmail.com>
2022-05-30 16:11:33 +0200

    refactor edit buttons

--- public/js/editButtonRedirect.js
+++ public/js/editButtonRedirect.js
@@ -2,7 +2,6 @@
 const editButtons = document.querySelectorAll('button.editButton');
 editButtons.forEach((editButton) => {
     editButton.addEventListener('click', () => {
-        //@ts-ignore
         window.location.href = `/edit/${editButton.value}`;
     });
 });
--- src/public/js/editButtonRedirect.ts
+++ src/public/js/editButtonRedirect.ts
@@ -1,8 +1,7 @@
-const editButtons = document.querySelectorAll('button.editButton');
+const editButtons: NodeListOf<HTMLButtonElement> = document.querySelectorAll('button.editButton');
 
 editButtons.forEach((editButton) => {
     editButton.addEventListener('click', () => {
-        //@ts-ignore
         window.location.href = `/edit/${editButton.value}`;
     })
 })
